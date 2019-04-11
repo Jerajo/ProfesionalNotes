cd302fdf48741066e637c5c0cddba971b731a8e4
Jesse R. Jose <jessejose@outlook.es>
2019-04-11 03:03:01 -0400

    Scripts agregados

---
+++ PN/Scripts/app.js
@@ -0,0 +1,12 @@
+﻿// Cambio de tema Oscuro | Claro
+function toggleStyleSheet() {
+    let style = document.getElementById("theme");
+    let styleSheet = style.getAttribute("href");
+
+    if (styleSheet == "/Content/dark-theme.css") style.setAttribute("href", "/Content/light-theme.css");
+    else style.setAttribute("href", "/Content/dark-theme.css");
+}
+
+function notYect() {
+    alert("Aun no programado t.t");
+}
