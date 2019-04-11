d656619b03b03a37fa1c3afdb0be8e8d22fad442
Jesse R. Jose <jessejose@outlook.es>
2019-04-11 03:04:00 -0400

    Proyecto de pruebas agregado

---
+++ PN.Tests/Models/DBFirst/AppDbContextTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PN.Models.Tests
+{
+    [TestClass]
+    public class AppDbContextTests
+    {
+        [TestMethod]
+        public async void StoredProcedureRegisterUserTest()
+        {
+            // Ini
+            AppDbContext db = new AppDbContext();
+
+            // Body
+            var resoult = await db.StoredProcedureRegisterUser("XxX", "m");
+
+            // Asset
+            Assert.IsTrue(resoult);
+        }
+    }
+}
