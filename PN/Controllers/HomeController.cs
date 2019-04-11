302e9a5567ba4c6dfeaf812c76357bf58331ea76
Jesse R. Jose <jessejose@outlook.es>
2019-04-11 03:12:04 -0400

    diseño del sitio actualizado

--- PN/Controllers/HomeController.cs
+++ PN/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PN.Models;
 using System.Linq;
-using System.Web;
+using PN.Services;
 using System.Web.Mvc;
 
 namespace PN.Controllers
@@ -10,20 +9,57 @@ namespace PN.Controllers
     {
         public ActionResult Index()
         {
+            using (var db = new AppDbContext())
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    var userService = new UserService();
+                    var userInfo = userService.GetUserInformation();
+                    ViewBag.LastRead = userInfo.LastPostId;
+
+                }
+
+                var forums = db.Forum.Take(6).ToList();
+                return View(forums);
+            }
+        }
+
+        // GET: Home/Subscribe/5
+        [Authorize]
+        public ActionResult Subscribe(int? id)
+        {
+            if (id == null) return HttpNotFound();
+
+            var userService = new UserService();
+            var userInfo = userService.GetUserInformation();
+
+            using (var db = new AppDbContext())
+            {
+                if (!db.UserForumSubscription.Any(o => o.UserId == userInfo.Id && o.ForumId == id.Value))
+                {
+                    var subscription = new UserForumSubscription
+                    {
+                        UserId = userInfo.Id,
+                        ForumId = id.Value,
+                        Score = 0
+                    };
+
+                    db.UserForumSubscription.Add(subscription);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            return HttpNotFound();
+        }
+
+        public ActionResult Edit()
+        {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Privacy()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
