0ace8529b8f0273cff609f05304fe15513393944
Jesse R. Jose <jessejose@outlook.es>
2019-04-11 03:01:28 -0400

    rutas actualizadas

--- PN/App_Start/RouteConfig.cs
+++ PN/App_Start/RouteConfig.cs
@@ -14,6 +14,30 @@ namespace PN
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ForumsName",
+                url: "Foros/{action}/{forumName}",
+                defaults: new { controller = "Forum", action = "Index", forumName = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "TagsName",
+                url: "Temas/{action}/{forumName}/{tagName}",
+                defaults: new { controller = "Tag", action = "Index", tagName = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "PostsName",
+                url: "Articulos/{action}/{forumName}/{tagName}/{postName}",
+                defaults: new { controller = "Post", action = "Index", postName = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "UserList",
+                url: "Usuarios/{id}",
+                defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
