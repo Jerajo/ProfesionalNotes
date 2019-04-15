5c569b8d82487525e4d86a987d47f43b77b61e99
Jesse R. Jose <jessejose@outlook.es>
2019-04-14 21:20:33 -0400

    actualizacion de paquetes

--- PN/Global.asax.cs
+++ PN/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PN.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace PN
     {
         protected void Application_Start()
         {
+            LanguagesService.RegisterLanguages();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
