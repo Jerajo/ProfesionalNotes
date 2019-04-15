657572fb749dec18a5d11c24a96661419af76ac7
Jesse R. Jose <jessejose@outlook.es>
2019-04-14 21:16:13 -0400

    Soporte multi idiomas agregado a Home controllers y views

--- PN/Controllers/HomeController.cs
+++ PN/Controllers/HomeController.cs
@@ -1,66 +1,33 @@
-﻿using PN.Models;
+﻿using System;
+using PN.Models;
+using System.Web;
+using System.Net;
 using System.Linq;
 using PN.Services;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace PN.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            using (var db = new AppDbContext())
+            IsLoading = await Loading();
+            var model = new HomeViewModel { Forums = db.Forum.Take(6).ToList() };
+
+            if (Request.IsAuthenticated)
             {
-                if (User.Identity.IsAuthenticated)
+                using (var userService = new UserService())
                 {
-                    var userService = new UserService();
                     var userInfo = userService.GetUserInformation();
-                    ViewBag.LastRead = userInfo.LastPostId;
-
-                }
-
-                var forums = db.Forum.Take(6).ToList();
-                return View(forums);
-            }
-        }
-
-        // GET: Home/Subscribe/5
-        [Authorize]
-        public ActionResult Subscribe(int? id)
-        {
-            if (id == null) return HttpNotFound();
-
-            var userService = new UserService();
-            var userInfo = userService.GetUserInformation();
-
-            using (var db = new AppDbContext())
-            {
-                if (!db.UserForumSubscription.Any(o => o.UserId == userInfo.Id && o.ForumId == id.Value))
-                {
-                    var subscription = new UserForumSubscription
-                    {
-                        UserId = userInfo.Id,
-                        ForumId = id.Value,
-                        Score = 0
-                    };
-
-                    db.UserForumSubscription.Add(subscription);
-                    db.SaveChanges();
-
-                    return RedirectToAction("Index");
+                    model.LastPostRead = db.Post.Find(userInfo.LastPostId);
                 }
             }
-            return HttpNotFound();
-        }
 
-        public ActionResult Edit()
-        {
-            return View();
-        }
-
-        public ActionResult Privacy()
-        {
-            return View();
+            return View(model);
         }
     }
 }
